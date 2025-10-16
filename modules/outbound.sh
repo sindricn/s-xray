9bcfc1f08f89b52047629611f3fbd1fc51cdc18c
sindricn <hua.sindri@gmail.com>
2025-10-16 22:15:32 +0800

    更新

--- modules/outbound.sh
+++ modules/outbound.sh
@@ -114,7 +114,11 @@ add_http_outbound() {
             protocol: "http",
             tag: $tag,
             settings: {
-                servers: [$server]
+                servers: [$server],
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8"
+                }
             }
         }')
 
